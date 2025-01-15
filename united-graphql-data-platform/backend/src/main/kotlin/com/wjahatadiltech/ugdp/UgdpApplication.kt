8e365a5fdd259764800d395c9107df36758035fb
Wajahat Adil <wajahatsyed1257@gmail.com>
2025-01-15 19:38:39 +0500

    feat: Started working on my first Kotlin project which is the Unified GraphQL Data Platform. The idea comes from service developed at Airbnb called Viaduct which provides a global schema & query system through a GraphQL interface

---
+++ united-graphql-data-platform/backend/src/main/kotlin/com/wjahatadiltech/ugdp/UgdpApplication.kt
@@ -0,0 +1,11 @@
+package com.wjahatadiltech.ugdp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UgdpApplication
+
+fun main(args: Array<String>) {
+	runApplication<UgdpApplication>(*args)
+}
---
+++ united-graphql-data-platform/backend/src/test/kotlin/com/wjahatadiltech/ugdp/UgdpApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wjahatadiltech.ugdp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UgdpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

da4aabb9e9ab395fc5414129bf01c7866f79faae
Wajahat Adil <wajahatsyed1257@gmail.com>
2025-01-16 17:03:42 +0500

    Quick changes in order to fix issues.

--- united-graphql-data-platform/backend/src/test/kotlin/com/wjahatadiltech/ugdp/UgdpApplicationTests.kt
+++ united-graphql-data-platform/backend/src/test/kotlin/com/wjahatadiltech/ugdp/UgdpApplicationTests.kt
@@ -1,15 +1,22 @@
 package com.wjahatadiltech.ugdp
 
+import com.wjahatadiltech.ugdp.entity.User
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import com.wjahatadiltech.ugdp.graphql.QueryResolver;
+import com.wjahatadiltech.ugdp.repository.PropertyRepository
+import com.wjahatadiltech.ugdp.repository.UserRepository
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
+import java.util.*
 
 @SpringBootTest
 class UgdpApplicationTests {
-	@Autowired
-	private lateinit var queryResolver: QueryResolver
+	private val userRepository: UserRepository = mock()
+	private val propertyRepository: PropertyRepository = mock()
+	private val queryResolver = QueryResolver(userRepository, propertyRepository)
 
 	@Test
 	fun contextLoads() {
@@ -17,9 +24,18 @@ class UgdpApplicationTests {
 
 	@Test
 	fun testGetUser() {
-		val user = queryResolver.getUser(1L)
-		assertEquals("John Doe", user.name)
+		// Arrange
+		val userId = 1L
+		val mockUser = User(id = userId, name = "Test User", email = "test_user@gmail.com")
+		whenever(userRepository.findById(userId)).thenReturn(Optional.of(mockUser))
+
+		// Act
+		val user = queryResolver.getUser(userId)
+
+		// Assert
+		assertEquals(mockUser, user)
 	}
+
 
 
 }

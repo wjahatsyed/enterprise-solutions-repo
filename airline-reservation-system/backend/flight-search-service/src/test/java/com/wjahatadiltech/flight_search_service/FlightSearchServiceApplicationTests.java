72d7fadd80432d6ada7a33e7171320496a22ad89
Wajahat Adil <wajahatsyed1257@gmail.com>
2025-01-17 17:26:21 +0500

    feat: Initialized flight-search-service

---
+++ airline-reservation-system/backend/flight-search-service/src/test/java/com/wjahatadiltech/flight_search_service/FlightSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wjahatadiltech.flight_search_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
